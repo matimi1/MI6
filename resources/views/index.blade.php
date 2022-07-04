915575f93b2ea7b5847357a79607e72bf5256504
Ilja M <i.meleshkevich@gmail.com>
2022-07-04 13:56:44 +0200

    script added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,5 +22,15 @@
       </div>
     </div>
     <main class="content"></main>
+    <script>
+        const button = document.querySelector(".nav-control button");
+const navPanel = document.querySelector(".nav-panel");
+
+button.addEventListener("click", () => {
+  navPanel.classList.toggle("hidden");
+  button.textContent = button.textContent == ">" ? "<" : ">";
+});
+
+    </script>
   </body>
 </html>
