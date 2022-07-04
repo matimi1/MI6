17d92091a84b597e6a5d831727d6c0affce88369
Ilja M <i.meleshkevich@gmail.com>
2022-07-04 16:12:28 +0200

    list with react added

--- resources/js/PeopleOfInterest/App.js
+++ resources/js/PeopleOfInterest/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-    return (
+    const [data, setData] = useState(null);
+
+    const fetchData = async () => {
+        const res = await fetch("/api/people-of-interest");
+        const parsedResponse = await res.json();
+        setData(parsedResponse);
+    };
+
+    useEffect(fetchData, []);
+
+    return data == null ? (
+        <h1>Loading...</h1>
+    ) : (
         <div className="welcome">
             <h1>Welcome to People of Interest REACT side</h1>
+            {data.map((person, i) => {
+                return (
+                    <p key={i}>
+                        {person.name} is {person.occupation}
+                    </p>
+                );
+            })}
         </div>
     );
 }
